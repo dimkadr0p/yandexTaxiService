c62c569b072d602bba58fb34a7752d4799de4c37
dimon <dmitrykhachidze@inbox.ru>
2023-04-22 19:12:20 +0300

    init

---
+++ src/main/java/ru/dmitry/yandexTaxi/YandexTaxiApplication.java
@@ -0,0 +1,13 @@
+package ru.dmitry.yandexTaxi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YandexTaxiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YandexTaxiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/dmitry/yandexTaxi/YandexTaxiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dmitry.yandexTaxi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YandexTaxiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
